eddce3bfa391fc76f678cb9c0460416813503ec3
Sebastian-byte <sebastianmontoya209@gmail.com>
2020-11-22 22:19:20 -0500

    Improvements

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='Python-ColorText',
-    packages=['ColorText'],
-    version='1.0',
-    license='GPL v3',
-    description='Color Strings in Python easily',
-    author='Sebastian-byte',
-    author_email='sebastianmontoya209@gmail.com',
-    url='https://github.com/Sebastian-byte/ColorText',
-    download_url='https://github.com/Sebastian-byte/ColorText/archive/v1.0.tar.gz',
-    keywords='color python text',
-    classifiers=['Programming Language :: Python',
-                 'Programming Language :: Python :: 3.3',
-                 'Programming Language :: Python :: 3.4',
-                 'Programming Language :: Python :: 3.5',
-                 'Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: 3.7',
-                 'Programming Language :: Python :: 3.8',
-                 'Programming Language :: Python :: 3.9'],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Python-ColorText",
+    version="1.0.1",
+    license="GPL v3",
+    author="Sebastian Montoya",
+    author_email="sebastianmontoya209@gmail.com",
+    description="Color Strings in Python easily",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sebastian-byte/ColorText",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
 )
